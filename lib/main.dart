3a1b86182179c776409b147c9cd740a81ffbe628
ju4ncito <villarrealjm2@gmail.com>
2023-06-10 15:59:11 -0300

    Mas carpetas. ApiTest funcionando. fixes #3

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sign_in_button/sign_in_button.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:turnero_taie_front/pages/login_page.dart';
+import 'package:turnero_taie_front/pages/api_test_page.dart';
+// import 'package:turnero_taie_front/pages/login_page.dart';
 import 'pages/tutor_page.dart';
 
 void main() {
@@ -106,7 +107,7 @@ class HomePage extends StatelessWidget {
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => LoginPage()),
+                        MaterialPageRoute(builder: (context) => ApiPage()),
                       );
                     },
                     child: Text('First Login'),
---
+++ lib/pages/api_test_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:turnero_taie_front/models/post.dart';
+import 'package:turnero_taie_front/services/remote_sv.dart';
+
+// https://jsonplaceholder.typicode.com/
+// https://app.quicktype.io/
+
+class ApiPage extends StatefulWidget {
+  const ApiPage({super.key});
+
+  @override
+  State<ApiPage> createState() => _ApiPageState();
+}
+
+class _ApiPageState extends State<ApiPage> {
+  List<Post>? posts;
+  var isLoaded = false;
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    posts = await RemoteService().getPosts();
+    if (posts != null) {
+      setState(() {
+        isLoaded = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ApiTest / Posts'),
+      ),
+      body: Visibility(
+        visible: isLoaded,
+        replacement: const Center(
+          child: CircularProgressIndicator(),
+        ),
+        child: ListView.builder(
+          itemCount: posts?.length,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.grey[300],
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          posts![index].title,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          posts![index].body,
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
