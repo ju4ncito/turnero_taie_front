f56b7f9a53bc8c353c6ccef8a644639f91084e76
ju4ncito <villarrealjm2@gmail.com>
2023-09-03 13:10:59 -0300

    Patch ya disponible en CRUD de schedules, refs #14
    falta implementar eliminar el horario y actualizar el home cada vez que lo visito no solo cuando cambio de calendar a home

--- lib/components/schedule_detail.dart
+++ lib/components/schedule_detail.dart
@@ -1,69 +1,209 @@
 import 'package:flutter/material.dart';
 import 'package:turnero_taie_front/swagger_generated_code/api_model.swagger.dart';
+import '../api/api_manager.dart';
 
-class ScheduleDetail extends StatelessWidget {
+class ScheduleDetail extends StatefulWidget {
   final TutorUserSchedule tutorSchedule;
 
-  const ScheduleDetail({Key? key, required this.tutorSchedule})
-      : super(key: key);
+  ScheduleDetail({required this.tutorSchedule});
+
+  @override
+  _ScheduleDetailState createState() => _ScheduleDetailState();
+}
+
+class _ScheduleDetailState extends State<ScheduleDetail> {
+  late TextEditingController dayController;
+  late TextEditingController modalityController;
+  late TextEditingController capacityController;
+  late TextEditingController beginController;
+  late TextEditingController endController;
+
+  late TutorUserSchedule editedTutorSchedule;
+  bool isEditing = false;
+
+  @override
+  void initState() {
+    super.initState();
+    editedTutorSchedule = widget.tutorSchedule;
+
+    // Initialize controllers with the current values
+    dayController = TextEditingController(text: editedTutorSchedule.day);
+    modalityController =
+        TextEditingController(text: editedTutorSchedule.modality);
+    capacityController =
+        TextEditingController(text: editedTutorSchedule.capacity.toString());
+    beginController = TextEditingController(text: editedTutorSchedule.begin);
+    endController = TextEditingController(text: editedTutorSchedule.end);
+  }
+
+  void enableEditing() {
+    setState(() {
+      isEditing = true;
+    });
+  }
+
+  void saveChanges() {
+    // Update the editedTutorSchedule with new values from controllers
+    editedTutorSchedule = TutorUserSchedule(
+      id: editedTutorSchedule.id,
+      day: dayController.text,
+      modality: modalityController.text,
+      capacity: int.parse(capacityController.text),
+      begin: beginController.text,
+      end: endController.text,
+      tutorUser: editedTutorSchedule.tutorUser,
+    );
+
+    // Implement logic to save the edited schedule here
+    // You can save the editedTutorSchedule to your backend or perform other actions
+
+    setState(() {
+      isEditing = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controllers
+    dayController.dispose();
+    modalityController.dispose();
+    capacityController.dispose();
+    beginController.dispose();
+    endController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detalle del Horario'),
+        title: Text('Schedule Detail'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Día: ${tutorSchedule.day}',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            Visibility(
+              visible: !isEditing,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Día: ${editedTutorSchedule.day}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    'Modalidad: ${editedTutorSchedule.modality}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    'Capacidad (asistentes): ${editedTutorSchedule.capacity.toString()}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    'Hora de Inicio: ${editedTutorSchedule.begin}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    'Hora de Finalización: ${editedTutorSchedule.end}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
             ),
-            SizedBox(height: 10),
-            Text(
-              'Modalidad: ${tutorSchedule.modality}',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Capacidad: ${tutorSchedule.capacity.toString()} asistentes',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Hora de Inicio: ${tutorSchedule.begin}',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Hora de Finalización: ${tutorSchedule.end}',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 20), // Espacio entre la información y los botones
+            if (isEditing)
+              Column(
+                children: [
+                  TextFormField(
+                    controller: dayController,
+                    decoration: InputDecoration(labelText: 'Día'),
+                  ),
+                  TextFormField(
+                    controller: modalityController,
+                    decoration: InputDecoration(labelText: 'Modalidad'),
+                  ),
+                  TextFormField(
+                    controller: capacityController,
+                    keyboardType: TextInputType.number,
+                    decoration:
+                        InputDecoration(labelText: 'Capacidad (asistentes)'),
+                  ),
+                  TextFormField(
+                    controller: beginController,
+                    decoration: InputDecoration(labelText: 'Hora de Inicio'),
+                  ),
+                  TextFormField(
+                    controller: endController,
+                    decoration:
+                        InputDecoration(labelText: 'Hora de Finalización'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final editedTutorSchedule = TutorUserSchedule(
+                        id: widget.tutorSchedule
+                            .id, // Asegúrate de incluir el ID del horario
+                        day: dayController.text,
+                        modality: modalityController.text,
+                        capacity: int.parse(capacityController.text),
+                        begin: beginController.text,
+                        end: endController.text,
+                        tutorUser: widget.tutorSchedule.tutorUser,
+                      );
+
+                      // Crear un objeto PatchedTutorUserScheduleRequest utilizando los valores editados
+                      final patchedRequest = PatchedTutorUserScheduleRequest(
+                        modality: editedTutorSchedule.modality,
+                        day: editedTutorSchedule.day,
+                        begin: editedTutorSchedule.begin,
+                        end: editedTutorSchedule.end,
+                        capacity: editedTutorSchedule.capacity,
+                        tutorUser: editedTutorSchedule.tutorUser,
+                      );
+
+                      print("Tutoria Request Body: ${patchedRequest.toJson()}");
+
+                      final apiManager = ApiManager();
+                      final postResult = await apiManager.apiModel
+                          .apiTutorUserSchedulesIdPatch(
+                        id: widget.tutorSchedule
+                            .id, // Pasar el ID del horario que se está modificando
+                        body:
+                            patchedRequest, // Usar el objeto PatchedTutorUserScheduleRequest
+                      );
+
+                      if (postResult.error == null) {
+                        saveChanges();
+                        // La solicitud PATCH se realizó con éxito
+                        print(
+                            "API Response Status Code: ${postResult.statusCode}");
+                        // Puedes realizar otras acciones o mostrar un mensaje de éxito aquí
+                      } else {
+                        // Error en la solicitud PATCH
+                        print(
+                            "Error en la solicitud PATCH: ${postResult.error}");
+                        // Puedes mostrar un mensaje de error o manejar la situación de error aquí
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                    ),
+                    child: Text('Guardar Cambios'),
+                  ),
+                ],
+              ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // Acción para modificar el horario
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blue,
+                if (!isEditing)
+                  ElevatedButton(
+                    onPressed: enableEditing,
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                    ),
+                    child: Text('Modificar Horario'),
                   ),
-                  child: Text('Modificar Horario'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // Acción para cancelar el horario
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red,
-                  ),
-                  child: Text('Cancelar Horario'),
-                ),
               ],
             ),
           ],
